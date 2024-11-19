c927415cc079c980a351b553606fefec873b66ec
Filzac <fiza0001@stud.kea.dk>
2024-11-19 10:52:15 +0100

    New branch with new hero

--- src/js/script.js
+++ src/js/script.js
@@ -8,17 +8,3 @@ document.addEventListener("DOMContentLoaded", () => {
     readMoreBtn.textContent = isExpanded ? "Læs Mere" : "Læs Mindre";
   });
 });
-
-document.addEventListener("DOMContentLoaded", () => {
-  const buttons = document.querySelectorAll(".carousel-btn");
-  buttons.forEach((button, index) => {
-    button.addEventListener("click", () => {
-      const dropdown = button
-        .closest(".item")
-        .querySelector(".dropdown-content");
-      const isExpanded = dropdown.classList.contains("show");
-      dropdown.classList.toggle("show");
-      button.textContent = isExpanded ? "LÆS MERE" : "LÆS MINDRE";
-    });
-  });
-});
